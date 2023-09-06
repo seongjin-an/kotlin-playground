a0203c141a4f95a9419414bc851612a4d8108a28
seongjin-an <seongjin4us@gmail.com>
2023-09-06 19:01:38 +0900

    스프링 웹플럭스. 그리고 웹클라이언트.

---
+++ spring-reactive-web/src/main/kotlin/com/ansj/springreactiveweb/SpringReactiveWebApplication.kt
@@ -0,0 +1,11 @@
+package com.ansj.springreactiveweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringReactiveWebApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringReactiveWebApplication>(*args)
+}
---
+++ spring-reactive-web/src/test/kotlin/com/ansj/springreactiveweb/SpringReactiveWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ansj.springreactiveweb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringReactiveWebApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

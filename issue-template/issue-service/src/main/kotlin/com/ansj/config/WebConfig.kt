ec52ae776c0ee7166e4767bcb2715c3ef57be9e2
seongjin-an <seongjin4us@gmail.com>
2023-09-10 16:57:34 +0900

    reactive app, issue-service 이슈관리 서비스와 회원 인증 서비스 연동

--- issue-template/issue-service/src/main/kotlin/com/ansj/config/WebConfig.kt
+++ issue-template/issue-service/src/main/kotlin/com/ansj/config/WebConfig.kt
@@ -1,5 +1,9 @@
 package com.ansj.config
 
+import com.ansj.exception.UnauthorizedException
+import com.fasterxml.jackson.annotation.JsonProperty
+import kotlinx.coroutines.runBlocking
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
@@ -7,6 +11,8 @@ import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 
 @Configuration
@@ -25,7 +31,9 @@ class WebConfig(
 }
 
 @Component
-class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
+class AuthUserHandlerArgumentResolver(
+    @Value("\${auth.url}") val authUrl: String,
+) : HandlerMethodArgumentResolver {
     // 컨트롤러 인자로 들어오는 특정 객체, 애노테이션에 대해서 supportsParameter 조건에 부합하면, resolveArgument 를 통해서 해당
     // 애노테이션 혹은 객체에 대해서 값을 세팅해주거나 저장해주도록 함.
     override fun supportsParameter(parameter: MethodParameter): Boolean {
@@ -38,16 +46,28 @@ class AuthUserHandlerArgumentResolver : HandlerMethodArgumentResolver {
         webRequest: NativeWebRequest,
         binderFactory: WebDataBinderFactory?
     ): Any? {
-        return AuthUser(
-            userId = 1,
-            username = "테스트",
 
-        )
+        val authHeader: String = webRequest.getHeader("Authorization") ?: throw UnauthorizedException()
+
+//        return AuthUser(
+//            userId = 1,
+//            username = "테스트",
+//        )
+        return runBlocking {
+            WebClient.create()
+                .get()
+                .uri(authUrl)
+                .header("Authorization",authHeader)
+                .retrieve()
+                .awaitBody<AuthUser>()
+        }
     }
 }
 
 data class AuthUser(
+    @JsonProperty("id")
     val userId : Long,
     val username : String,
-    val profileUrl : String?= null,
+    val email: String,
+    val profileUrl : String? = null,
 )
